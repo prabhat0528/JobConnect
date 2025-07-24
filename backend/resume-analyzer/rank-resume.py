80b2898a26131a438d2b7613fdecae3dd4501766
prabhat0528 <prabhatrai0528@gmail.com>
2025-07-24 10:56:54 +0530

    Added host=0.0.0.0 for fixing deployment error

--- backend/resume-analyzer/rank-resume.py
+++ backend/resume-analyzer/rank-resume.py
@@ -129,4 +129,4 @@ def analyze_resume():
         return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, host = '0.0.0.0',debug=True)
